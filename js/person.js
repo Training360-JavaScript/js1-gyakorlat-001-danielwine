ad5039bf3dba8508c98bcbcb5dd48182183bebf4
daniel <danielwine@tutamail.com>
2021-10-27 15:40:43 +0200

    Finally...

--- js/person.js
+++ js/person.js
@@ -1,4 +1,4 @@
 
-const personDataLog = (firstname = 'john', lastname = 'doe', age = '33') => {
+const personDataLog = ({firstname = 'john', lastname = 'doe', age = '33'} = {}) => {
     return `My name is ${firstname} ${lastname}. I'm ${age} years old.`
 }
