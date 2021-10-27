c0336c120666cfe62c76808fb46d036411d1a1d5
daniel <danielwine@tutamail.com>
2021-10-27 12:29:00 +0200

    Create arrow functions.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+const handleClick = () => {
+    let buttons = document.querySelectorAll("button")
+    for (let i = 0; i < 3; i++) {
+        buttons[i].addEventListener('click', function() {
+            console.log(this.innerHTML);
+        })
+    }
+}
+
+handleClick()
---
+++ js/person.js
@@ -0,0 +1,4 @@
+
+const personDataLog = (firstname = 'john', lastname = 'doe', age = '33') => {
+    return `My name is ${firstname} ${lastname}. I'm ${age} years old.`
+}
